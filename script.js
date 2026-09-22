c9a96958f8016a5c78d3869df361f688d003bac0
Rafaela Januario Silva <rafacienciacomput@gmail.com>
2026-09-21 21:34:25 -0300

    Add button click event to display message

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = 
+    document.getElementById("botao");
+
+const mensagem = 
+    document.getElementById("mensagem");
+
+botao.addEventListener("click", function (){
+    mensagem.textContent = "Funcionou! JavaScript está rodando (amassei 💅).";
+});
